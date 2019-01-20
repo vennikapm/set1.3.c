facd61eb1cee73d753c236fef443afbd8f2fb014
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-19 22:51:15 -0800

    Create hello2.c

---
+++ hello2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+char value;
+printf("enter testcase");
+scanf("%d",&n");
+while(n>0)
+{
+printf("enter value");
+scanf("%c",&value);
+if(value!=a&&value!=e&&value!=i&&value!=o&&value!=u)
+{
+printf("consonant");
+}
+else
+{
+printf("vowel");
+}
+n--;
+}
+getch();
+}
